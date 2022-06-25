ec9e970e4823d549a4fb62643f0beaa2cd700c3d
bramara_ <bramara.sambada.17@gmail.com>
2022-06-25 15:08:15 +0700

    mencoba code variabel

--- JSVariables.js
+++ JSVariables.js
@@ -1,3 +1,11 @@
-let x = "10" + 10 + 10;
-let y = 10 + 10 + "10";
-console.log(x + " X & Y " + y)
+// let x = "10" + 10 + 10;
+// let y = 10 + 10 + "10";
+// console.log(x + " X & Y " + y)
+
+//materi js variabel
+// let length = 16; //Number
+// let lastName = "Johnson" //String
+// const x = {firstname:"John",lastname:"Doe"}; //Object
+// console.log(typeof length)
+// console.log(typeof lastName)
+// console.log(typeof x)
